19dbd8fdafa7440e2230f1b5c92d0cdae12990ac
Aozorany <aoisorani@Aozorany-Mac-Mini.local>
2014-12-15 17:48:13 +0800

    Add a static lib target

---
+++ libStaticCollectionView/libStaticCollectionView.h
@@ -0,0 +1,13 @@
+//
+//  libStaticCollectionView.h
+//  libStaticCollectionView
+//
+//  Created by Aoisorani on 12/15/14.
+//  Copyright (c) 2014 Douliu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libStaticCollectionView : NSObject
+
+@end
---
+++ libStaticCollectionView/libStaticCollectionView.m
@@ -0,0 +1,13 @@
+//
+//  libStaticCollectionView.m
+//  libStaticCollectionView
+//
+//  Created by Aoisorani on 12/15/14.
+//  Copyright (c) 2014 Douliu. All rights reserved.
+//
+
+#import "libStaticCollectionView.h"
+
+@implementation libStaticCollectionView
+
+@end
